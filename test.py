6718acf14b7200afbe1e0dcce5ef0ce828311ab5
marinajp-hub <marinajp@usc.edu>
2026-03-04 17:27:48 -0800

    new column workers.txt

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-print("hello world")
+# print("hello world")
 
-myList = ['banana', 'apple', 'pear']
-myList.append('blueberry')
+# myList = ['banana', 'apple', 'pear']
+# myList.append('blueberry')
 
-print(myList[3])
+# print(myList[3])
+
+
+import pandas as pd
+df = pd.read_csv("Workers.txt", sep="\s+", skiprows=2)
+print(df.describe())
+df['Gary Sunchem Ratio'] = df['Gary'] / df['Sunchem']
+print(df['Gary Sunchem Ratio'])
